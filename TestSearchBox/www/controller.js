b6ae88f048ad4f6c8fed39c6d3068996e25b5846
Nic 7aller <n.7aller@gmail.com>
2016-11-19 14:42:38 -0500

    Moved function to controller

--- TestSearchBox/www/controller.js
+++ TestSearchBox/www/controller.js
@@ -1,4 +1,22 @@
 ﻿angular.module('TestApp', [])
     .controller('TestAppController', function($scope) {
-        
+        $scope.initAutocomplete = function () {
+            var input = document.getElementById('pac-input');
+            var searchBox = new google.maps.places.SearchBox(input);
+
+            searchBox.addListener('places_changed', function () {
+                var places = searchBox.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+
+                places.forEach(function (place) {
+                    if (!place.geometry) {
+                        console.log("Returned place contains no geometry");
+                        return;
+                    }
+                });
+            });
+        };
 });
